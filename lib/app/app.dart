43b10759420b8b1e329c9abaad7b94cb2fcc24fd
KhaledRamadan880 <khaledramdan880@gmail.com>
2024-09-20 03:49:14 +0200

    Create Project

---
+++ lib/app/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UIMushafApp extends StatelessWidget {
+  const UIMushafApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:iu_mushaf/app/app.dart';
+
+void main() {
+  runApp(const UIMushafApp());
+}
