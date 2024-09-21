4e22f16700ca6559b103b60447260cd656f3e589
KhaledRamadan880 <khaledramdan880@gmail.com>
2024-09-22 01:38:03 +0200

    Remove Unused Import

--- lib/features/settings/presentation/views/settings_view.dart
+++ lib/features/settings/presentation/views/settings_view.dart
@@ -1,5 +1,4 @@
 import 'package:iu_mushaf/core/imports/imports.dart';
-import 'package:iu_mushaf/core/widgets/custom_gradient_body.dart';
 import 'package:iu_mushaf/features/settings/presentation/widgets/setting_view_body.dart';
 import 'package:iu_mushaf/features/settings/presentation/widgets/setting_view_header.dart';
 
