4835eea67764de8b06c54c4df2ffb1d246b6f421
KhaledRamadan880 <khaledramdan880@gmail.com>
2024-09-26 03:51:43 +0200

    Fix Splash

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -18,7 +18,7 @@ class _SplashViewState extends State<SplashView> {
   void initState() {
     super.initState();
     showButtons = widget.showButtons!;
-    if (sl<Cache>().getBoolData("IsFirstStart") == true) {
+    if (sl<Cache>().getBoolData("IsFirstStart") != false) {
       Future.delayed(
         const Duration(seconds: 2),
         () {
